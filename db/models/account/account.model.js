aeec483b6f8bbe3f2806ea326e0f60da087f96aa
Pedro J. Pimentel <pedro@pimentel.co>
2018-01-24 14:57:05 -0200

    Merge branch 'dev' of github.com:rafa-thayto/ava-gama-banking into dev

--- db/models/account/account.model.js
+++ db/models/account/account.model.js
@@ -3,4 +3,14 @@ const collectionName = 'accounts';
 const modelName = 'Account';
 const schema = require('./account.schema');
 
+schema.statics.findTransactionsByAccount = function findTransactionsByAccount(agencia, accountNumber) {
+    return this.findOne({ account_number: accountNumber, ag: agencia })
+               .lean()
+               .then(account => this.model("Transaction").find({
+                        $or: [
+                          {from: account._id}, {to: account._id}
+                         ]
+                     }).populate('from to'))
+}
+
 module.exports = mongoose.model(modelName, schema, collectionName);
--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -19,7 +19,8 @@ import { LogoutComponent } from './app.components/auth/logout/logout.component';
 import { HeaderMenuComponent } from './app.components/header-menu/header-menu.component';
 import { SideMenuComponent } from './app.components/side-menu/side-menu.component';
 import { DashboardComponent } from './app.components/dashboard/dashboard.component';
-import { SearchComponent } from './search/search.component';
+import { AccountService } from '../app/app.services/account.service';
+import { SearchComponent } from './app.components/search/search.component';
 
 
 
@@ -48,7 +49,7 @@ import { SearchComponent } from './search/search.component';
     HttpModule
     
   ],
-  providers: [],
+  providers: [AccountService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
