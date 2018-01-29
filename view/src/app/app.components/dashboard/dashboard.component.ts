fb56456fe117c1abd41e33bb3f86decfeaeb1730
Pedro J. Pimentel <pedro@pimentel.co>
2018-01-29 11:40:24 -0200

    [add] dashboar dloading

--- view/src/app/app.components/dashboard/dashboard.component.ts
+++ view/src/app/app.components/dashboard/dashboard.component.ts
@@ -11,9 +11,14 @@ import 'rxjs/add/operator/mergeMap';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  public loading: boolean;
   public lastTransactions: Observable<ITransaction[]>
 
   constructor(private authService: AuthService, private transactionService: TransactionService) {
-    this.lastTransactions = this.authService.account.flatMap(account => this.transactionService.find({ ag: account.ag, account_number: account.account_number, limit: 10 }));
+    this.lastTransactions = this.authService.account
+      .do(() => this.loading = true)
+      .flatMap(account => this.transactionService.find({ ag: account.ag, account_number: account.account_number, limit: 10 }))
+      .delay(1000)
+      .do(() => this.loading = false);
   }
 }
