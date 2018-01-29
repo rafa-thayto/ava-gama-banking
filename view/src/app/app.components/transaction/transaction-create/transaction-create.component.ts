7c55b1029a26d2c60d31325d064fc30108872488
Rafael Thayto <rafa.thayto@gmail.com>
2018-01-29 11:26:43 -0200

    [update] Atualizando transaction-create component

--- view/src/app/app.components/transaction/transaction-create/transaction-create.component.ts
+++ view/src/app/app.components/transaction/transaction-create/transaction-create.component.ts
@@ -10,23 +10,21 @@ import { Observable } from 'rxjs/Observable';
 })
 export class TransactionCreateComponent implements OnInit {
 
+  
+  private minhaConta: string
+  private agencia: string
+  private conta: string
+  private valorTransferido: string
+  private senha: string
+  
+  // Material Steps
   private firstFormGroup: FormGroup
   private secondFormGroup: FormGroup
   private isLinear = true
 
-  agencia: number
-  conta: number
-  valorTransferido: number
-  
-  //mascara numero da agência
-  public maskAgencia = [/[0-9]/, /\d/, /\d/, /\d/]
-
-  //mascara numero da conta
-  public maskConta = [/[0-9]/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/]
-
-  public valorTranferido() {
-    console.log(this.valorTranferido);
-  }
+  // Masks
+  // public maskAgencia = [/[0-9]/, /\d/, /\d/, /\d/]
+  // public maskConta = [/[0-9]/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/]
 
   constructor(private service: TransactionService, private fb: FormBuilder) {
   }
@@ -43,10 +41,10 @@ export class TransactionCreateComponent implements OnInit {
       'agencia': ['' , Validators.compose([Validators.required])],
       'conta': ['' , Validators.compose([Validators.required])],
       'valorTransferido': ['' , Validators.compose([Validators.required])]
-
     });
+
     this.secondFormGroup = this.fb.group({
-      secondCtrl: ['', Validators.required]
+      'senha': ['', Validators.required]
     });
   }
 
